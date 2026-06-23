2f6f33db559a077a4fc3bb6d8fe940374eabcf44
BoopathyVelanK <boopathyvelan8@gmail.com>
2026-06-23 16:17:33 +0530

    Sort Colors code uploaded

---
+++ Arrays/Sort Colors.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int zero = 0, one = 0, two = 0;
+
+        for(int x : nums){
+            if(x == 0) zero++;
+            else if(x == 1) one++;
+            else two++;
+        }
+
+        int i = 0;
+
+        while(zero-- > 0) nums[i++] = 0;
+        while(one-- > 0)  nums[i++] = 1;
+        while(two-- > 0)  nums[i++] = 2;
+    }
+}
