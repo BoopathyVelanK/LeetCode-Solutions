4e6a8e04c3162adaefdbf48d9d5c52df11e435f0
BoopathyVelanK <boopathyvelan8@gmail.com>
2026-06-23 16:13:33 +0530

    Find Numbers with Even Number of Digits code uploaded

---
+++ Math/Find Numbers with Even Number of Digits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+         int k=(int)Math.log10(nums[i])+1;
+         if(k%2==0){
+            c++;
+         }
+        }
+        return c;
+    }
+}
